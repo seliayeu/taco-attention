99d9dac419ef393f1b5a1f75acca537bf9f0b02e
Danila Seliayeu <dan.seliayeu@gmail.com>
2025-04-07 22:37:32 -0600

    update code structure, add notebook plots

---
+++ scripts/run-all.sh
@@ -0,0 +1,22 @@
+for sparsity in $(seq 0.0 0.05 0.95)
+do
+    LD_LIBRARY_PATH=../taco/build/lib/ python scripts/run.py ./a.out \
+        --n_q 512 --n_k 512 --d_k 1024 --d_v 1024 --sparsity $sparsity --QFormat DD --KFormat DD --VFormat DD --sample 1 --convert 0 --runs 10 \
+        >> scripts/results/baseline-1-0.txt
+
+    LD_LIBRARY_PATH=../taco/build/lib/ python scripts/run.py ./a.out \
+        --n_q 512 --n_k 512 --d_k 1024 --d_v 1024 --sparsity $sparsity --QFormat CSR --KFormat CSC --VFormat CSC --sample 1 --convert 1 --runs 10 \
+        >> scripts/results/csr-csc-csc-1-1.txt
+
+    LD_LIBRARY_PATH=../taco/build/lib/ python scripts/run.py ./a.out \
+        --n_q 512 --n_k 512 --d_k 1024 --d_v 1024 --sparsity $sparsity --QFormat CSR --KFormat CSC --VFormat DD --sample 1 --convert 1 --runs 10 \
+        >> scripts/results/csr-csc-dd-1-1.txt
+
+    LD_LIBRARY_PATH=../taco/build/lib/ python scripts/run.py ./a.out \
+        --n_q 512 --n_k 512 --d_k 1024 --d_v 1024 --sparsity $sparsity --QFormat DD --KFormat CSC --VFormat CSC --sample 1 --convert 1 --runs 10 \
+        >> scripts/results/dd-csr-csc-1-1.txt
+
+    LD_LIBRARY_PATH=../taco/build/lib/ python scripts/run.py ./a.out \
+        --n_q 512 --n_k 512 --d_k 1024 --d_v 1024 --sparsity $sparsity --QFormat CSR --KFormat DD --VFormat CSC --sample 1 --convert 1 --runs 10 \
+        >> scripts/results/csr-dd-csc-1-1.txt
+done
